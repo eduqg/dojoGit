ceb044fc6878abf808fd9480e26d2122acbd0edf
Tayh <tayhcarvalho@gmail.com>
2017-03-16 11:49:30 -0300

    Add soma

--- soma.c
+++ soma.c
@@ -1,8 +1,16 @@
 /*Receba dois números, some-os e imprima o resultado*/
 #include <stdlib.h>
-#include <abc.h>
+#include <math.h>
 
 int main(){
+	int a, b;
+	int soma;
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	soma = a+b;
+
+	printf("%d", soma);
     return 0;
 }
 
